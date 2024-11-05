6cd5b096dd22d5ac6573bffab5f11030024e571c
annaw0016 <agustinawulandari825@gmail.com>
2024-11-05 15:39:40 +0700

    Delete Penjualan-0016 directory

--- Penjualan-0016/hapus_brg.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'koneksi.php';
-
-$no = $_GET['no'];
-$conn->query("DELETE FROM produk WHERE no = $no");
-
-header("Location: barang.php");
-?>
--- Penjualan-0016/hapus_pelanggan.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'koneksi.php';
-
-$no = $_GET['no'];
-$conn->query("DELETE FROM pelanggan WHERE no = $no");
-
-header("Location: pelanggan.php");
-?>
